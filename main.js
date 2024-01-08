f64c5d3b4771ded31315b7e9eabae605fa3bde4f
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2024-01-08 09:33:05 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "src/js/preload.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+};
+app.whenReady().then(() => {
+  createWindow();
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
